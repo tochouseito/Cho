96dc50b786a0ea3aa22d2844877830df39d551eb
朝聖 屋良 <k023g0060@m.neec.ac.jp>
2024-10-01 03:16:22 +0900

    DSVの設定

--- project/Cho/System/D3D12/DSVManager/DSVManager.cpp
+++ project/Cho/System/D3D12/DSVManager/DSVManager.cpp
@@ -1,2 +1,67 @@
 #include "PrecompiledHeader.h"
 #include "DSVManager.h"
+#include"WinApp/WinApp.h"
+#include"D3D12/D3DDevice/D3DDevice.h"
+
+void DSVManager::Initialize(WinApp* win, D3DDevice* d3dDevice)
+{
+	win_ = win;
+	d3dDevice_ = d3dDevice;
+
+	// デスクリプタヒープ作成
+	descriptorHeap_ = d3dDevice_->CreateDescriptorHeap(
+		HEAP_TYPE,
+		1,
+		false
+	);
+	// デスクリプタサイズ取得
+	descriptorSize_ = d3dDevice->GetDevice()->GetDescriptorHandleIncrementSize(
+		HEAP_TYPE
+	);
+
+	CreateDepthStencil();
+}
+
+void DSVManager::CreateDepthStencil()
+{
+	resource_ = CreateDepthStencilTextureResource(
+		win_->kClientWidth,
+		win_->kClientHeight
+	);
+}
+
+Microsoft::WRL::ComPtr<ID3D12Resource> DSVManager::CreateDepthStencilTextureResource(int32_t width, int32_t height)
+{
+	// 生成するResourceの設定
+	D3D12_RESOURCE_DESC resourceDesc{};
+	resourceDesc.Width = width;// Textureの幅
+	resourceDesc.Height = height;// Textureの高さ
+	resourceDesc.MipLevels = 1;//mipmapの数
+	resourceDesc.DepthOrArraySize = 1;// 奥行き or 配列Textureの配列数
+	resourceDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;// DepthStencilとして利用可能なフォーマット
+	resourceDesc.SampleDesc.Count = 1;// サンプリングカウント。1固定。
+	resourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;// 2次元
+	resourceDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;// DepthStencilとして使う通知
+
+	// 利用するHeapの設定
+	D3D12_HEAP_PROPERTIES heapProperties{};
+	heapProperties.Type = D3D12_HEAP_TYPE_DEFAULT;// VRAM上に作る
+
+	// 深度値のクリア設定
+	D3D12_CLEAR_VALUE depthClearValue{};
+	depthClearValue.DepthStencil.Depth = 1.0f;// 1.0f（最大値）でクリア
+	depthClearValue.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;// フォーマット。Resourceと合わせる
+
+	// Resourceの生成
+	HRESULT hr;
+	Microsoft::WRL::ComPtr < ID3D12Resource> resource = nullptr;
+	hr = d3dDevice_->GetDevice()->CreateCommittedResource(
+		&heapProperties,// Heapの設定
+		D3D12_HEAP_FLAG_NONE,// Heapの特殊な設定。特になし
+		&resourceDesc,// Resourceの設定
+		D3D12_RESOURCE_STATE_DEPTH_WRITE,// 深度値を書き込む状態にしておく
+		&depthClearValue,// Clear最適値
+		IID_PPV_ARGS(&resource));// 作成するResourceポインタへのポインタ
+	assert(SUCCEEDED(hr));
+	return resource;
+}
--- project/Cho/System/D3D12/DSVManager/DSVManager.h
+++ project/Cho/System/D3D12/DSVManager/DSVManager.h
@@ -1,4 +1,11 @@
 #pragma once
+
+#include<d3d12.h>
+#include<wrl.h>
+#include<cstdint>
+
+class WinApp;
+class D3DDevice;
 class DSVManager
 {
 public:
@@ -6,6 +13,38 @@ public:
 	/// <summary>
 	/// 初期化
 	/// </summary>
-	void Initialize();
+	void Initialize(WinApp* win,D3DDevice* d3dDevice);
+
+private:// メンバ関数
+
+	/// <summary>
+	/// 深度リソースの作成
+	/// </summary>
+	void CreateDepthStencil();
+
+	/// <summary>
+	/// 深度リソースの作成
+	/// </summary>
+	Microsoft::WRL::ComPtr < ID3D12Resource	> CreateDepthStencilTextureResource(
+		int32_t width, int32_t height
+	);
+
+private:
+	/*借りインスタンス*/
+	WinApp* win_ = nullptr;
+	D3DDevice* d3dDevice_ = nullptr;
+
+	// デスクリプタサイズ
+	uint32_t descriptorSize_;
+
+	// デスクリプタヒープ
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> descriptorHeap_;
+
+	// ヒープタイプ
+	static const D3D12_DESCRIPTOR_HEAP_TYPE HEAP_TYPE =
+		D3D12_DESCRIPTOR_HEAP_TYPE::D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+
+	// 深度リソース
+	Microsoft::WRL::ComPtr < ID3D12Resource>resource_;
 };
 
