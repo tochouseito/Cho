9d57c1c83cbb0b15971ca35fe29b5688899b8002
朝聖 屋良 <k023g0060@m.neec.ac.jp>
2024-10-06 20:46:58 +0900

    フレームレート固定完了

--- project/Cho/System/FrameRate/FrameRate.h
+++ project/Cho/System/FrameRate/FrameRate.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <windows.h>
 #include <iostream>
 #include<chrono>
 #include<thread>
@@ -22,16 +21,9 @@ public:// メンバ関数
 
 private: // メンバ変数
 
-	void Wait();
 
 	/*記録時間FPS固定用*/
 	std::chrono::steady_clock::time_point reference_;
-	LARGE_INTEGER liPerfFreq;  // パフォーマンスカウンターの周波数
-	LARGE_INTEGER liPerfCount; // パフォーマンスカウンターの現在値
-	double previousTime;       // 前回のフレームの時間（マイクロ秒単位）
-	double targetFrameTime;    // 1フレームの目標時間（マイクロ秒単位）
-	double accumulatedTimeError; // フレームレート補正用の誤差の蓄積
-	int targetFPS;// 目標フレームレート
 
 	GameContext* gameContext_ = nullptr;
 };
