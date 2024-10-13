7127a901bdb560bd2832494e0ac6b5f11a2d8cf2
朝聖 屋良 <k023g0060@m.neec.ac.jp>
2024-10-13 14:11:12 +0900

    Meshクラス開始

--- project/Cho/System/Generator/Mesh/Mesh.h
+++ project/Cho/System/Generator/Mesh/Mesh.h
@@ -1,5 +1,28 @@
 #pragma once
+
+// Utility
+#include"Base/Vector2.h"
+#include"Base/Vector3.h"
+#include"Base/Vector4.h"
+
 class Mesh
 {
+public:
+	// 頂点データ構造体（テクスチャあり）
+	struct VertexData {
+		Vector4 position;
+		Vector2 texcoord;
+		Vector3 normal;
+	};
+	enum class Object {
+		kBox,
+		kPlane,
+		kSphere,
+	};
+	struct MeshData {
+		VertexData* vertexData;
+	};
+private:
+
 };
 
