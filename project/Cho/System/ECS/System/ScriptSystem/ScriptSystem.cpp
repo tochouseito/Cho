b5c37eef5aff692a49876e13523b73ca8f75fc90
tochouseito <k023g0060@g.neec.ac.jp>
2024-12-04 13:02:26 +0900

    修正

--- project/Cho/System/ECS/System/ScriptSystem/ScriptSystem.cpp
+++ project/Cho/System/ECS/System/ScriptSystem/ScriptSystem.cpp
@@ -4,19 +4,18 @@
 void ScriptSystem::Update(EntityManager& entityManager, ComponentManager& componentManager)
 {
     for (Entity entity : entityManager.GetActiveEntities()) {
-        PhysicsComponent* physics = componentManager.GetPhysics(entity);
-        TransformComponent* transform = componentManager.GetTransform(entity);
-
-        // 後で消す
-       /* CameraComponent* cameraCompo = componentManager.GetCamera(entity);*/
-        if (physics && transform) {
-            // 物理計算: 位置の更新
-            physics->velocity.x += physics->acceleration.x * deltaTime;
-            transform->translation.x += physics->velocity.x * deltaTime;
-            // 他の軸も同様に処理
-        } else if (!physics && transform)
-        {
-            transform->UpdateMatrix();
-        }
+       // PhysicsComponent* physics = componentManager.GetPhysics(entity);
+       // TransformComponent* transform = componentManager.GetTransform(entity);
+        ScriptComponent* script = componentManager.GetScript(entity);
+        script;
+       // // 後で消す
+       ///* CameraComponent* cameraCompo = componentManager.GetCamera(entity);*/
+       // if (physics && transform) {
+       //     // 物理計算: 位置の更新
+       //     // 他の軸も同様に処理
+       // } else if (!physics && transform)
+       // {
+       //     transform->UpdateMatrix();
+       // }
     }
 }
