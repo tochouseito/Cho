5603b9552f86c77c56dad480a9833ed443ada979
朝聖 屋良 <k023g0060@m.neec.ac.jp>
2024-12-22 12:36:03 +0900

    デバッグ用にプロジェクト選択画面をスキップ

--- project/Cho/System/StartSetting/StartSetting.cpp
+++ project/Cho/System/StartSetting/StartSetting.cpp
@@ -70,80 +70,87 @@ bool StartSetting::IsProject()
 
 void StartSetting::SelectedProject()
 {
-    FindProjects();
-    // モーダルウィンドウを表示するフラグ
-    if (!ImGui::IsPopupOpen("Project Selector"))
     {
-        ImGui::OpenPopup("Project Selector");
+        // エンジンデバッグのため
+        // ロード機能が実装されるまで
+        isProject = true;
+        createNewProject = true;
+        projectName = "test";
     }
+    //FindProjects();
+    //// モーダルウィンドウを表示するフラグ
+    //if (!ImGui::IsPopupOpen("Project Selector"))
+    //{
+    //    ImGui::OpenPopup("Project Selector");
+    //}
 
-    // モーダルウィンドウのスタイル
-    ImGuiWindowFlags windowFlags = ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize |
-        ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar;
+    //// モーダルウィンドウのスタイル
+    //ImGuiWindowFlags windowFlags = ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize |
+    //    ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar;
 
-    // モーダルウィンドウの表示
-    if (ImGui::BeginPopupModal("Project Selector", nullptr, windowFlags))
-    {
-        // ウィンドウを中央に配置
-        ImGui::SetNextWindowPos(ImGui::GetMainViewport()->GetCenter(), ImGuiCond_Always, ImVec2(0.5f, 0.5f));
-        ImGui::SetNextWindowSize(ImVec2(400, 300));
+    //// モーダルウィンドウの表示
+    //if (ImGui::BeginPopupModal("Project Selector", nullptr, windowFlags))
+    //{
+    //    // ウィンドウを中央に配置
+    //    ImGui::SetNextWindowPos(ImGui::GetMainViewport()->GetCenter(), ImGuiCond_Always, ImVec2(0.5f, 0.5f));
+    //    ImGui::SetNextWindowSize(ImVec2(400, 300));
 
-        // ラジオボタンでプロジェクトの作成か読み込みを選択
-        if (ImGui::RadioButton("Create New Project", createNewProject)) {
-            createNewProject = true;
-        }
-        if (ImGui::RadioButton("Load Existing Project", !createNewProject)) {
-            createNewProject = false;
-        }
+    //    // ラジオボタンでプロジェクトの作成か読み込みを選択
+    //    if (ImGui::RadioButton("Create New Project", createNewProject)) {
+    //        createNewProject = true;
+    //    }
+    //    if (ImGui::RadioButton("Load Existing Project", !createNewProject)) {
+    //        createNewProject = false;
+    //    }
 
-        ImGui::Spacing();
+    //    ImGui::Spacing();
 
-        // プロジェクト名の入力
-        ImGui::Text("Project Name:");
-        ImGui::InputText("##ProjectName", projectNameBuffer, sizeof(projectNameBuffer));
-        projectName = projectNameBuffer; // 入力内容を std::string に保存
+    //    // プロジェクト名の入力
+    //    ImGui::Text("Project Name:");
+    //    ImGui::InputText("##ProjectName", projectNameBuffer, sizeof(projectNameBuffer));
+    //    projectName = projectNameBuffer; // 入力内容を std::string に保存
 
-        ImGui::Spacing();
+    //    ImGui::Spacing();
 
-        // プロジェクト名が空の場合ボタンを無効化
-        bool isNameValid = !projectName.empty();
+    //    // プロジェクト名が空の場合ボタンを無効化
+    //    bool isNameValid = !projectName.empty();
 
-        if (createNewProject) {
-            if (!isNameValid) {
-                ImGui::BeginDisabled(); // ボタンを無効化
-            }
-            if (ImGui::Button("Create Project", ImVec2(120, 0))) {
-                isProject = true;
-                ImGui::CloseCurrentPopup(); // モーダルを閉じる
-            }
-            if (!isNameValid) {
-                ImGui::EndDisabled(); // ボタンを再度有効化
-            }
-        } else {
-            if (!isNameValid) {
-                ImGui::BeginDisabled(); // ボタンを無効化
-            }
-            if (ImGui::Button("Load Project", ImVec2(120, 0))) {
-                isProject = true;
-                ImGui::CloseCurrentPopup(); // モーダルを閉じる
-            }
-            if (!isNameValid) {
-                ImGui::EndDisabled(); // ボタンを再度有効化
-            }
-        }
+    //    if (createNewProject) {
+    //        if (!isNameValid) {
+    //            ImGui::BeginDisabled(); // ボタンを無効化
+    //        }
+    //        if (ImGui::Button("Create Project", ImVec2(120, 0))) {
+    //            isProject = true;
+    //            ImGui::CloseCurrentPopup(); // モーダルを閉じる
+    //        }
+    //        if (!isNameValid) {
+    //            ImGui::EndDisabled(); // ボタンを再度有効化
+    //        }
+    //    } else {
+    //        if (!isNameValid) {
+    //            ImGui::BeginDisabled(); // ボタンを無効化
+    //        }
+    //        if (ImGui::Button("Load Project", ImVec2(120, 0))) {
+    //            isProject = true;
+    //            ImGui::CloseCurrentPopup(); // モーダルを閉じる
+    //        }
+    //        if (!isNameValid) {
+    //            ImGui::EndDisabled(); // ボタンを再度有効化
+    //        }
+    //    }
 
-        ImGui::SeparatorText("Projects");
+    //    ImGui::SeparatorText("Projects");
 
-        for (auto& project : projectList) {
-            if (ImGui::Selectable(project.c_str())) {
-                isProject = true;
-                createNewProject = false;
-                ImGui::CloseCurrentPopup(); // モーダルを閉じる
-            }
-        }
+    //    for (auto& project : projectList) {
+    //        if (ImGui::Selectable(project.c_str())) {
+    //            isProject = true;
+    //            createNewProject = false;
+    //            ImGui::CloseCurrentPopup(); // モーダルを閉じる
+    //        }
+    //    }
 
-        ImGui::EndPopup();
-    }
+    //    ImGui::EndPopup();
+    //}
 }
 
 void StartSetting::CreateProject()
